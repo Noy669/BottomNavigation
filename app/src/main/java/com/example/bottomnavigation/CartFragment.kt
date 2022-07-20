e80c80dc40e8e213df0cf8c579b16c8d54b5d413
Noy Kheranyan <kheranyan.noy@mail.ru>
2022-07-20 19:31:25 +0400

    Add TabLayout and ViewPager final, add nav drawer

--- app/src/main/java/com/example/bottomnavigation/CartFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/CartFragment.kt
@@ -5,17 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.bottomnavigation.adapter.PagerAdapter
 import com.example.bottomnavigation.databinding.FragmentCartBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class CartFragment : Fragment() {
 
-    private lateinit var binding: FragmentCartBinding
+    private lateinit var binding:FragmentCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentCartBinding.inflate(layoutInflater)
-        initial()
+
     }
 
     private fun initial() {
@@ -32,14 +32,15 @@ class CartFragment : Fragment() {
         }.attach()
     }
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(layoutInflater, container, false)
+        initial()
+        return binding.root
     }
 
 }
+
+
--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -1,49 +1,67 @@
 package com.example.bottomnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNav: BottomNavigationView
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNav = findViewById(R.id.bottom_nav)
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.bottom_home -> {
-                    loadFragment(HomeFragment())
-                    true
-                }
-                R.id.bottom_search -> {
-                    loadFragment(SearchFragment())
-                    true
-                }
-                R.id.bottom_cart -> {
-                    loadFragment(CartFragment())
-                    true
-                }
-                R.id.bottom_profile -> {
-                    loadFragment(PersonFragment())
-                    true
-                }
-                R.id.bottom_more -> {
-                    loadFragment(MoreFragment())
-                    true
-                }
-            }
-            true
-        }
+//          appBarConfiguration = AppBarConfiguration(
+//            setOf(R.id.cart_layout)
+//                    bottomNav.setupWithNavController (nav_host)}
+        initView()
+        bottomNav.setupWithNavController(navController)
+
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
-            .addToBackStack("")
-            .commit()
+    fun initView() {
+        bottomNav = findViewById(R.id.bottom_nav)
+        navController = findNavController(R.id.fragment_container)
     }
+//        bottomNav = findViewById(R.id.bottom_nav)
+//        bottomNav.setOnItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.bottom_home -> {
+//                    loadFragment(HomeFragment())
+//                    true
+//                }
+//                R.id.bottom_search -> {
+//                    loadFragment(SearchFragment())
+//                    true
+//                }
+//                R.id.bottom_cart -> {
+//                    loadFragment(CartFragment())
+//                    true
+//                }
+//                R.id.bottom_profile -> {
+//                    loadFragment(PersonFragment())
+//                    true
+//                }
+//                R.id.bottom_more -> {
+//                    loadFragment(MoreFragment())
+//                    true
+//                }
+//            }
+//            true
+//        }
+//    }
+//
+//    private fun loadFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.container, fragment)
+//            .addToBackStack("")
+//            .commit()
+//    }
+
 }
